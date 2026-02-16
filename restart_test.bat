ac504a46bf9d69c1e3497444e7c69745f239ca77
TraglerT <traglert@tutanota.com>
2026-02-16 13:20:06 +0100

    Script Default with New pytorch version

--- restart_test.bat
+++ restart_test.bat
@@ -3,14 +3,14 @@ set dataset=cub
 set model=ViT-B16
 set backbone=resnet18_cub
 set n_iters_interpretability_cutoff=1
-set interpretability_cutoff=0.15
-set conceptFile=random_words_200
-set extraSettings=--print --n_iters 5000 --clip_cutoff 0.20 --interpretability_cutoff %interpretability_cutoff% --lam 0.0006 --n_iters_interpretability_cutoff %n_iters_interpretability_cutoff%
+set interpretability_cutoff=0.45
+set conceptFile=filtered
+set extraSettings=--n_iters 5000 --clip_cutoff 0.26 --interpretability_cutoff %interpretability_cutoff% --lam 0.0006 --n_iters_interpretability_cutoff %n_iters_interpretability_cutoff%
 
 IF %deleteConceptActivations%==1 (
 	del saved_activations\%dataset%_%conceptFile%_%model%.pt
 )
-call conda activate BAThesis
+call conda activate BSThesisNewtorch
 set PYTHONUTF8=1
 python train_cbm.py --dataset %dataset% --backbone %backbone% --feature_layer features.final_pool --concept_set data/concept_sets/%dataset%_%conceptFile%.txt %extraSettings%
 pause
